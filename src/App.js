5165dfea96a07adc37f6fd4b7508f1275afe9aac
Lucas Siqueira <lucassiqzro@gmail.com>
2020-10-21 12:32:57 -0300

    Create base structure
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Co-authored-by: Guilherme Aguiar <gui9627oli@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TaskList from './components/TaskList';
+
+import { Container } from './styles/globalStyles';
+
+function App() {
+  return (
+    <Container>
+      <TaskList />
+    </Container>
+  );
+}
+
+export default App;
